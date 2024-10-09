3d13d3e19e1fad7147dccb36405be5974ef252dc
Danil Ibatullin <danil_rinatovich@list.ru>
2024-10-10 00:30:54 +0500

    first commit

---
+++ module_3_2_calling_function.py
@@ -0,0 +1,15 @@
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    end_name = ('.com', '.ru', '.net')
+    if '@' not in sender or '@' not in recipient or not sender.endswith(end_name) or not recipient.endswith(end_name):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
