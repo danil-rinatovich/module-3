afaf1182b11aac4a9e1eefea7ac570287c7c73f9
Danil Ibatullin <danil_rinatovich@list.ru>
2024-10-12 20:29:17 +0500

    no zero

--- module_3_5_recursion.py
+++ module_3_5_recursion.py
@@ -1,5 +1,5 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(number).replace('0', '')
     first = int(str_number[0])
     if len(str_number) > 1:
         return  first * get_multiplied_digits(int(str_number[1:]))
@@ -7,5 +7,5 @@ def get_multiplied_digits(number):
         return first
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(402030)
 print(result)
